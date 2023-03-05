13b5bae4abc0c32d2dc33cae4b55838dfd9b1654
Til Pöhnitzsch <til.poehnitzsch@gmail.com>
2023-03-05 23:35:26 +0100

    Update services.php
    add missing services

--- src/Resources/config/services.php
+++ src/Resources/config/services.php
@@ -3,13 +3,18 @@
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use Feierstoff\ToolboxBundle\ApiGenerator\ApiDocGenerator;
+use Feierstoff\ToolboxBundle\ApiGenerator\Parameter\InjectParameters;
+use Feierstoff\ToolboxBundle\Serializer\Serializer;
 
 return function(ContainerConfigurator $container) {
 
     $container->services()
         ->set(ApiDocGenerator::class)
-        ->arg("\$router", service("router.default"))
-        ->alias("feierstoff_toolbox.service.api_doc_generator", ApiDocGenerator::class)
+            ->arg("\$router", service("router.default"))
+            ->alias("feierstoff_toolbox.service.api_doc_generator", ApiDocGenerator::class)
+        ->set(Serializer::class)
+            ->alias("feierstoff_toolbox.service.serializer", Serializer::class)
+        ->set(InjectParameters::class)
+            ->alias("feierstoff_toolbox.service.inject_parameters", InjectParameters::class)
     ;
-
 };
