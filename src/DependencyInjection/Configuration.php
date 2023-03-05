9fe6f60e6938eff10022d37c4e7cfde5121990aa
Til Pöhnitzsch <til.poehnitzsch@gmail.com>
2023-03-05 22:33:27 +0100

    init

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Feierstoff\ToolboxBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface {
+
+    public function getConfigTreeBuilder() {
+        // name: parent key in config/toolbox.yaml file
+        $treeBuilder = new TreeBuilder("toolbox");
+
+        // define config tree
+        $rootNode = $treeBuilder->getRootNode();
+/*        $rootNode
+            ->children()
+            ->scalarNode("user_entity")->defaultValue("App\Entity\User")->end()
+            ->scalarNode("twig_fallback_path")->defaultValue("app.html.twig")->end()
+            ->scalarNode("auth_method")
+            ->defaultValue("session")
+            ->validate()
+            ->ifNotInArray(["oauth", "session"])
+            ->thenInvalid("Invalid auth method.")
+            ->end()
+            ->end()
+            ->arrayNode("mailer")
+            ->isRequired()
+            ->children()
+            ->arrayNode("sender")
+            ->isRequired()
+            ->children()
+            ->scalarNode("mail")->isRequired()->end()
+            ->scalarNode("name")->isRequired()->end()
+            ->end()
+            ->end()
+            ->arrayNode("send")
+            ->children()
+            ->scalarNode("exception")->defaultValue(null)->end()
+            ->end()
+            ->end()
+            ->end()
+            ->end()
+        ;*/
+
+        return $treeBuilder;
+    }
+
+}
---
+++ src/DependencyInjection/ToolboxExtension.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Feierstoff\ToolboxBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+
+class ToolboxExtension extends Extension {
+
+    public function load(array $configs, ContainerBuilder $container) {
+        $loader = new PhpFileLoader($container, new FileLocator(__DIR__."/../Resources/config"));
+        $configuration = $this->getConfiguration($configs, $container);
+        // array one can access to get the values from the config file
+        $config = $this->processConfiguration($configuration, $configs);
+
+        /* --- CONTROLLERS --- */
+/*        $loader->load("controllers.php");
+
+        $fallbackRoute = $container->getDefinition(FallbackController::class);
+        $fallbackRoute->setArgument("\$conf_twig_fallback_path", $config["twig_fallback_path"]);*/
+
+
+        /* --- LISTENERS --- */
+/*        $loader->load("listeners.php");
+        $accessControlListener = $container->getDefinition(AccessControlListener::class);
+        $accessControlListener->setArgument("\$conf_user_entity", $config["user_entity"]);
+        $exceptionListener = $container->getDefinition(ExceptionListener::class);
+        $exceptionListener->setArgument("\$config", $config);*/
+
+
+
+        /* --- COMMANDS --- */
+/*        $loader->load("commands.php");*/
+
+
+
+        /* --- SERVICES --- */
+/*        $loader->load("services.php");
+
+        $authenticator = $container->getDefinition(Authenticator::class);
+        $authenticator->setArgument("\$conf_auth_method", $config["auth_method"]);*/
+
+
+    }
+
+}
